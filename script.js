297aefec4a3544ed5c15e0ffb628bc6d658cdc9e
eang-minea <eangminea68@gmail.com>
2026-06-18 16:54:55 +0700

    update_ui

---
+++ script.js
@@ -0,0 +1,61 @@
+var images = [
+    "c:\Users\USER\Documents\ASP.Net.webp",
+    "c:\Users\USER\Documents\ASP.Net.webp",
+    "c:\Users\USER\Documents\ASP.Net.webp",
+    "c:\Users\USER\Documents\ASP.Net.webp",
+    "c:\Users\USER\Documents\ASP.Net.webp",
+    "c:\Users\USER\Documents\ASP.Net.webp",
+    "c:\Users\USER\Documents\ASP.Net.webp",
+    "c:\Users\USER\Documents\ASP.Net.webp",
+];
+
+
+var firstCard = null
+var secondCard = null
+var canFlip = true
+var matches = 0
+var moves = 0
+var secomds = 0
+var timeRunning = false
+var timerInterval;
+
+
+function startgame() {
+    var gameBoard = document.getElementById("gameBoard")
+    gameBoard.innerHTML = ""
+
+    var cardImages = images.concat(images) 
+
+    cardImages.sort(function() {
+        return Math.random() - 0.5
+    })
+
+    for (var i = 0; i < cardImages.length; i++) {
+        var card = document.createElement('div');
+        card.className = "card";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
